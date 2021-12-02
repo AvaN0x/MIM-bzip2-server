4668ea7a5ede566e05f04c4c60d63a5ffd3baf91
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-12-02 12:46:17 +0100

    comment every function + add printListElement

--- src/shared/huffman/list.h
+++ src/shared/huffman/list.h
@@ -1,8 +1,6 @@
 #ifndef LIST_H
 #define LIST_H
 
-#include <stdbool.h>
-
 #include "TA.h"
 #include "node.h"
 #include <stdio.h>
@@ -12,7 +10,6 @@ typedef struct elem_list_t
 {
 	node_t *n;
 	struct elem_list_t *suc;
-	struct elem_list_t *pred;
 } list_t;
 
 typedef struct
@@ -21,33 +18,92 @@ typedef struct
 	node_t *min2;
 } minNodes_t;
 
-list_t *
-listCons(node_t *N, list_t *L);
+/**
+ * @brief Construct a list element from a node
+ *
+ * @param N The node
+ * @return list_t* The list element created
+ */
+list_t *listConstruct(node_t *N);
 
+/**
+ * @brief Create a copy of the list element
+ *
+ * @param L The list element to copy
+ * @return list_t* The copy
+ */
+list_t *listCpy(list_t *L);
+
+/**
+ * @brief Constructor of an empty list
+ *
+ * @return list_t*
+ */
 list_t *emptyListCons();
 
-void setNode(list_t *L, node_t *N);
-
-bool isEmptyList(list_t *L);
-
-node_t *listHead(list_t *L);
-
-list_t *listRest(list_t *L);
-
-list_t *insNode(node_t *N, list_t *L);
-
+/**
+ * @brief Free a listElement and goto next one
+ *
+ * @param L The list element to free
+ * @return list_t* Next value
+ */
 list_t *destroyList(list_t *L);
 
+/**
+ * @brief Remove 2 mins from the list and replace it by their parent
+ *
+ * @param L list_t* The list where we remove the mins
+ * @param minNodes minNodes_t* Structure containing the mins
+ * @param nNode node_t* The new node to insert
+ *
+ * @return list_t* The new head of the list
+ */
 list_t *removeMins(list_t *L, minNodes_t *minNodes, node_t *nNode);
 
+/**
+ * @brief Get the 2 min nodes
+ *
+ * @param L list_t* The list to search the mins
+ * @return minNodes_t
+ */
 minNodes_t getMin(list_t *L);
 
+/**
+ * @brief Print the value of the node + suc
+ *
+ * @param L The list element to be transformed
+ * @return char* The string
+ */
 char *toStringList(list_t *N);
 
+/**
+ * @brief Print only the list element given
+ *
+ * @param L The list element to be printed
+ */
+void printListElement(list_t *N);
+
+/**
+ * @brief Print each value of the list from the element given
+ *
+ * @param L The first value to be printed
+ */
 void printList(list_t *N);
 
-void getCode(node_t *L, FILE *encodeFile);
+/**
+ * @brief Recursiv function to build the code of every node of the tree
+ *
+ * @param node The root of the tree
+ * @param huffmanDico The dictionnary to save the code
+ */
+void getCode(node_t *node, char **encodeFile);
 
+/**
+ * @brief Take a string to reverse it
+ *
+ * @param code The string to reverse
+ * @return char* The reversed string
+ */
 char *reverseCode(char *code);
 
 #endif
