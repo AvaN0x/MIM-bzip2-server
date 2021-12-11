d0112f920886f9bb5e154ebd1f732f2de3a4014b
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-12-11 11:54:10 +0100

    Comment functions + add encode + decode TODO

--- src/shared/huffman/huff.h
+++ src/shared/huffman/huff.h
@@ -3,6 +3,32 @@
 
 #include <stdio.h>
 
-void encodeHuffman(int *dico, char **HuffmanDico);
+/**
+ * @brief Construct the code for each caracter with more than 1 appearance buy building Huffman tree
+ *
+ * @param frequencies The frequency of each caracter in ASCII table from a string
+ * @param HuffmanDico The dictionnary than will be updated with the code of each caracter as a string
+ */
+void buildCodeHuffman(int *frequencies, char **HuffmanDico);
+
+/**
+ * @brief Encode a string with a dictionnary
+ *
+ * @param str The string to encode
+ * @param HuffmanDico The dictionnary
+ *
+ * @return char* The encoded string
+ */
+char *encodeHuffman(char *str, char **HuffmanDico);
+
+/**
+ * @brief Decode a string with a dictionnary
+ *
+ * @param str The string to decode
+ * @param HuffmanDico The dictionnary
+ *
+ * @return char* The decoded string
+ */
+char *decodeHuffman(char *str, char **HuffmanDico);
 
 #endif
