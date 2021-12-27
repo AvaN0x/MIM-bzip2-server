a1d09aab7bd51090a9b159d57563ce25c6e0e7fa
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2021-12-27 17:10:35 +0100

    update huffman test according to last commit

--- src/server/server.c
+++ src/server/server.c
@@ -17,10 +17,10 @@
 int main(int argc, char const *argv[])
 {
     // TODO when the full bzip2 encode and decode is working
-    if (0)
+    // if (0)
     {
         processFile("lorem_ipsum.txt");
-        processFile("mississippi.txt");
+        // processFile("mississippi.txt");
     }
     //----------------------//
     // Read content of file //
@@ -37,6 +37,8 @@ int main(int argc, char const *argv[])
         printf("tmpFileName: %s\n", tmpFileName);
         FILE *file = fopen(tmpFileName, "r");
         free(tmpFileName);
+        tmpFileName = NULL;
+        globalCounter--;
 
         if (!file)
         {
@@ -141,12 +143,12 @@ int main(int argc, char const *argv[])
     //--------------//
     // Test huffman //
     //--------------//
-    // if (0)
+    if (0)
     {
         printf(FONT_YELLOW "ENCODE HUFFMAN\n" FONT_DEFAULT);
         // const char *S = "exemple,de,codage,de,huffman";
-        const char *S = "Bonjour je m'appelle Luc et je veux tester mes fonctions parce que sinon je ne sais pas si cela fonctionne correctment.";
-
+        // const char *S = "Bonjour je m'appelle Luc et je veux tester mes fonctions parce que sinon je ne sais pas si cela fonctionne correctment.";
+        const char *S = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sed mauris non augue accumsan venenatis. Vivamus maximus luctus erat non vestibulum. Interdum et malesuada fames ac ante ipsum primis in faucibus. In vitae arcu congue, vehicula nulla nec, porta odio. Donec tempus nisi sed massa tempor, ut convallis lectus tempor. In lectus mauris, sollicitudin sed orci ut, eleifend faucibus diam. In condimentum dolor nec mauris pharetra tempor. In faucibus scelerisque eros eget bibendum. Maecenas pretium cursus";
         int dico[128] = {};
         char *HuffmanDico[128] = {};
 
@@ -162,7 +164,7 @@ int main(int argc, char const *argv[])
         // printf(FONT_YELLOW "AFTER BUILD HUFFMAN CODE GLOBAL COUNT IS %d\n" FONT_DEFAULT, globalCounter);
 
         // Print non null codes created
-        if (0)
+        // if (0)
         {
             for (int i = 0; i < 128; i++)
                 if (HuffmanDico[i] != NULL)
@@ -170,7 +172,9 @@ int main(int argc, char const *argv[])
         }
 
         // ------ ENCODE THE STRING ------ //
-        unsigned char *encoded = encodeHuffman(S, HuffmanDico);
+        unsigned char *encodedHuffman;
+        int encodedHuffmanSize;
+        encodedHuffman = encodeHuffman(S, len, HuffmanDico, &encodedHuffmanSize);
 
         // memory management debug
         // printf(FONT_YELLOW "AFTER ENCODE GLOBAL COUNT IS %d\n" FONT_DEFAULT, globalCounter);
@@ -180,25 +184,30 @@ int main(int argc, char const *argv[])
          * format-hex path_to_file/huffmanEncoded.bin
          * file is in the res folder
          */
+
         // Print decimal values of encoded string
-        printf(FONT_YELLOW "\nencoded is " FONT_DEFAULT);
-        int lgthEncoded = strlen((char *)encoded);
-        for (int i = 0; i < lgthEncoded; i++)
-            printf("%u ", encoded[i]);
+        printf(FONT_YELLOW "\nencoded is \n" FONT_DEFAULT);
+        for (int i = 0; i < encodedHuffmanSize; i++)
+            printf("%u ", encodedHuffman[i]);
         printf("\n");
 
         // ------ DECODE THE STRING ------ //
-        char *decoded = decodeHuffman(encoded, HuffmanDico);
-        printf(FONT_YELLOW "\ndecoded is %s\n" FONT_DEFAULT, decoded);
+        char *decodedHuffman;
+        int decodedHuffmanSize;
+        decodedHuffman = decodeHuffman(encodedHuffman, encodedHuffmanSize, HuffmanDico, &decodedHuffmanSize);
+        printf(FONT_YELLOW "\ndecoded is \n" FONT_DEFAULT);
+        for (int i = 0; i < decodedHuffmanSize; i++)
+            printf("%c", decodedHuffman[i]);
+        printf("\n");
 
         // ------ FREE MEMORY ------ //
-        free(encoded);
+        free(encodedHuffman);
         globalCounter--;
-        encoded = NULL;
+        encodedHuffman = NULL;
 
-        free(decoded);
+        free(decodedHuffman);
         globalCounter--;
-        decoded = NULL;
+        decodedHuffman = NULL;
 
         // Free codes in HuffmanDico
         for (int i = 0; i < 128; i++)
@@ -217,8 +226,8 @@ int main(int argc, char const *argv[])
         // Stats
         printf(FONT_YELLOW "\nStats\n" FONT_DEFAULT);
         printf("Initial string was occupying %ld bytes\n", len * sizeof(char));
-        printf("Encoded string was occupying %ld bytes\n", lgthEncoded * sizeof(unsigned char));
-        printf("Ration of compression is %f%%\n", (float)lgthEncoded / (float)len * 100);
+        printf("Encoded string was occupying %ld bytes\n", encodedHuffmanSize * sizeof(unsigned char));
+        printf("Ration of compression is %d%%\n", 100 * encodedHuffmanSize / len);
     }
 
     if (0)
